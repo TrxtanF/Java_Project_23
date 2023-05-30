a3fdbeb71b9655183f2de07b2e4d26b4949961ac
Tristan Finke <alphapvp654@gmail.com>
2023-05-30 18:48:04 +0200

    Create Model Student

--- Models/Student.java
+++ Models/Student.java
@@ -1,7 +1,37 @@
 package Models;
 
 public class Student {
-    public static void main(String[] args) {
-            System.out.println("Test");
+    private int Student_Id;
+    private String name;
+    private int Employer_Id;
+
+    public Student(int Student_Id, String name, int Employer_Id){
+        this.Student_Id = Student_Id;
+        this.name = name;
+        this.Employer_Id = Employer_Id;
+    }
+
+    public int getStudent_Id() {
+        return Student_Id;
+    }
+
+    public void setStudent_Id(int student_Id) {
+        Student_Id = student_Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployer_Id() {
+        return Employer_Id;
+    }
+
+    public void setEmployer_Id(int employer_Id) {
+        Employer_Id = employer_Id;
     }
 }
